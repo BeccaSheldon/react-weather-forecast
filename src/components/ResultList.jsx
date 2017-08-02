edf011205075af9f9636ce914359aac8e5724cd2
Rebecca Sheldon <rebecca.e.sheldon@gmail.com>
2017-08-02 16:40:08 -0700

    Correct variable names and format date and time

--- src/components/ResultList.jsx
+++ src/components/ResultList.jsx
@@ -1,20 +1,31 @@
 import React, {Component} from 'react'
-import Row from './Row.jsx'
+import DateFormat from 'dateformat'
 import ResultItem from './ResultItem.jsx'
+import Row from './Row.jsx'
 
 export default class ResultList extends Component {
+	formatDateTime(datestamp, timestamp) {
+		let date = DateFormat(datestamp, 'dddd, mmmm d')
+		let time = DateFormat(datestamp, 'h:MM TT')
+		return date + ' ' + time
+	}
+
+	formatTemperature(temperature) {
+		return Math.floor((temperature * 1.8) - 459.67)
+	}
+
 	render() {
 		return(
 			<Row className="Results">
 				{this.props.results.map((result, index) => (
 					<ResultItem {...{
-						date: result.dt_txt,
+						date: this.formatDateTime(result.dt_txt, result.dt),
 						key: index,
 						description: result.weather[0].description,
 						humidity: result.main.humidity,
-						temp: result.main.temp,
-						temp_max: result.main.temp_max,
-						temp_min: result.main.temp_min
+						temp: this.formatTemperature(result.main.temp),
+						temp_max: this.formatTemperature(result.main.temp_max),
+						temp_min: this.formatTemperature(result.main.temp_min)
 				  }}/>
 				))}
 			</Row>
