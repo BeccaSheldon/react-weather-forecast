02781e7fe69e8ac559f7c43487ffec441d13a9c2
Rebecca Sheldon <rebecca.e.sheldon@gmail.com>
2017-08-02 16:36:22 -0700

    Add wrapper div to make sticky footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
-import Forecast from './Forecast.jsx';
-import NavHeader from './NavHeader.jsx';
-import NavFooter from './NavFooter.jsx';
+import React, {Component} from 'react'
+import Forecast from './Forecast.jsx'
+import NavFooter from './NavFooter.jsx'
+import NavHeader from './NavHeader.jsx'
 
 export default class App extends Component {
 	render() {
 		return(
 			<div className="App">
-				<NavHeader />
-				<Forecast />
+				<div className="Wrapper">
+					<NavHeader />
+					<Forecast />
+				</div>
 				<NavFooter />
 			</div>
 		);
