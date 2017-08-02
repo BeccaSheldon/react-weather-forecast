4e44e1c7f64ab14145f70363ee8e9706513d926e
Rebecca Sheldon <rebecca.e.sheldon@gmail.com>
2017-08-02 16:40:38 -0700

    Add icons and remove words

--- src/components/ResultItem.jsx
+++ src/components/ResultItem.jsx
@@ -5,12 +5,12 @@ export default class ResultItem extends Component {
 		return (
 			<div className="Result-item">
 				<ul>
-					<li className="Result-item-heading">Forecast for: {this.props.date} </li>
-					<li>Description: {this.props.description}</li>
-					<li>Humidity: {this.props.humidity}</li>
-					<li>Temperature: {this.props.temp}</li>
-					<li>Max: {this.props.temp_max}</li>
-					<li>Min: {this.props.temp_min}</li>
+					<li className="Result-item-heading">{this.props.date}</li>
+					<li className="Result-item-subheading">{this.props.description}</li>
+					<li className="Result-item-row"><i className="fa fa-thermometer"></i> {this.props.temp}</li>
+					<li className="Result-item-row"><i className="fa fa-tint"></i> {this.props.humidity}</li>
+					<li className="Result-item-row"><i className="fa fa-angle-double-up"></i> {this.props.temp_max}</li>
+					<li className="Result-item-row"><i className="fa fa-angle-double-down"></i> {this.props.temp_min}</li>
 				</ul>
 			</div>
 		)
