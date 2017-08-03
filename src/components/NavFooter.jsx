22bd39ec391050d2068b897d922568690d156296
Rebecca Sheldon <rebecca.e.sheldon@gmail.com>
2017-08-03 16:05:50 -0700

    Use Row component

--- src/components/NavFooter.jsx
+++ src/components/NavFooter.jsx
@@ -1,43 +1,49 @@
 import React, {Component} from 'react'
 import Link from './Link.jsx'
+import Row from './Row.jsx'
 
 export default class NavFooter extends Component {
 	render() {
 		return(
-			<div className="Footer">
-				<div className="Row">
-					<ul className="Navbar-items">
-						<li>
-							<Link
-								target="_blank"
-								text="LinkedIn"
-								url="http://linkedin.com/rebeccasheldon"
-							/>
-						</li>
-						<li>
-							<Link
-								target="_blank"
-								text="Website"
-								url="http://cremedelacode.com"
-							/>
-						</li>
-						<li>
-							<Link
-								target="_blank"
-								text="Twitter"
-								url="http://twitter.com/beccasheldon"
-							/>
-						</li>
-						<li>
-							<Link
-								target="_blank"
-								text="Github"
-								url="http://github.com/beccasheldon"
-							/>
-						</li>
-					</ul>
-				</div>
-			</div>
+			<Row className="Footer">
+				<ul className="Navbar-items">
+					<li>
+						<Link
+							target="_blank"
+							text="LinkedIn"
+							url="http://linkedin.com/rebeccasheldon"
+						/>
+					</li>
+					<li>
+						<Link
+							target="_blank"
+							text="Website"
+							url="http://cremedelacode.com"
+						/>
+					</li>
+					<li>
+						<Link
+							target="_blank"
+							text="Twitter"
+							url="http://twitter.com/beccasheldon"
+						/>
+					</li>
+					<li>
+						<Link
+							target="_blank"
+							text="Github"
+							url="http://github.com/beccasheldon"
+						/>
+					</li>
+					<li>
+						<Link
+							className="pulse"
+							text="Email"
+							url="mailto:rebecca.e.sheldon@gmail.com"
+						></Link>
+					</li>
+				</ul>
+			</Row>
 		)
 	}
 }
