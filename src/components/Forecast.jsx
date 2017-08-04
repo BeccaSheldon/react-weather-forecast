e791ba9e1bfb1f83725f102bcfacd5881cfe0116
Rebecca Sheldon <rebecca.e.sheldon@gmail.com>
2017-08-03 21:11:29 -0700

    Add loding spinner to forecast component

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,19 +1,25 @@
 import React, {Component} from 'react'
-import Search from './Search.jsx'
+import Loading from './Loading.jsx'
 import ResultList from './ResultList.jsx'
+import Search from './Search.jsx'
 
 export default class Forecast extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			animate: false,
 			city: '',
-			results: [],
-      resultsTotal: ''
+			loading: false,
+			results: []
 		}
 	}
 
+	clearInput() {
+		console.log("How do we clear inputs in React?")
+	}
+
 	handleSubmit() {
+	  this.setState({loading: true})
+
     if(this.state.city !== '') {
   		let key = 'APPID=b2139d07e99d08bc22c314f37e836e7d'
   		let api = 'http://api.openweathermap.org/data/2.5/forecast/daily?'
@@ -21,7 +27,11 @@ export default class Forecast extends Component {
   	  fetch(url)
   	  .then(result => result.json())
   	  .then((data) => {
-      	this.setState({results: data.list})
+      	this.setState({
+      		loading: false,
+      		results: data.list
+      	})
+      	this.clearInput
   	  })
   	  .catch(err => new Error(console.log('Hit a snag: ' + err)))
     }
@@ -39,9 +49,13 @@ export default class Forecast extends Component {
 					clickHandler={this.handleSubmit.bind(this)}
 					value={this.state.city}
 				/>
-				<ResultList
-					results={this.state.results}
-				/>
+				{this.state.loading && <Loading />}
+				{!this.state.loading &&
+					<ResultList
+						city={this.state.city}
+						results={this.state.results}
+					/>
+				}
 			</div>
 		)
 	}
